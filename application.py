b81041a3d9604636712227bbcb83b8fab87a8f6b
EricYang <ericyang@hyenatek.com>
2016-07-19 17:58:31 +0800

    modify

--- application.py
+++ application.py
@@ -1,22 +1,16 @@
-from flask import Flask, request
+from flask import Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-todos = {}
 
+class HelloWorld(Resource):
 
-class TodoSimple(Resource):
-    
-    def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
+    def get(self):
+        return {'hello': 'world'}
 
-    def put(self, todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
-
-api.add_resource(TodoSimple, '/<string:todo_id>')
+api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
     app.run(debug=True)
