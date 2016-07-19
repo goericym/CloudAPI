41dc3f9e0e039699f6a549506259e5902a6fea0e
EricYang <ericyang@hyenatek.com>
2016-07-19 13:23:37 +0800

    add

--- application.py
+++ application.py
@@ -1,36 +1,22 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Resource, Api
 
-# print a nice greeting.
+app = Flask(__name__)
+api = Api(app)
+
+todos = {}
 
 
-def say_hello(username="World"):
-    return '<p>Hello %s!</p>\n' % username
+class TodoSimple(Resource):
+    
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
 
-# some bits of text for the page.
-header_text = '''
-    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
-instructions = '''
-    <p><em>Hint</em>: This is a RESTful web service! Append a username
-    to the URL (for example: <code>/Thelonious</code>) to say hello to
-    someone specific.</p>\n'''
-home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = '</body>\n</html>'
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
 
-# EB looks for an 'application' callable by default.
-application = Flask(__name__)
+api.add_resource(TodoSimple, '/<string:todo_id>')
 
-# add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
-
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
-
-# run the app.
-if __name__ == "__main__":
-    # Setting debug to True enables debug output. This line should be
-    # removed before deploying a production app.
-    application.debug = True
-    application.run()
+if __name__ == '__main__':
+    app.run(debug=True)
